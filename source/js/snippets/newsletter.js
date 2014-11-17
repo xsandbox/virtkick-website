26bb1b62af824f1a9db6f85770b41ed62902e43a
m13 <m13@balloonstudio.pl>
2014-11-17 22:50:28 +0100

    switch from bootstrap modal to cover-pop and add animation

--- source/js/snippets/newsletter.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function () {
-  $('#newsletter-modal .close').click(function () {
-    $('#newsletter-modal').hide();
-  });
-  $('#newsletter-modal').show();
-}, 10000);
--- source/js/virtkick.js
+++ source/js/virtkick.js
@@ -1,3 +1,4 @@
 //= require vendor/jquery-2.1.0.min.js
 //= require vendor/jquery.ajaxchimp-1.3.0.js
+//= require vendor/cover-pop.js
 //= require bootstrap
